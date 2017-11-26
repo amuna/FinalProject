7fe85c96d0dfc6c219a65c01416da79ca2e03de5
Ahmed Naeem <ahmed.naeem@uoit.net>
2017-11-25 20:27:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/amuna95/finalproject/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.amuna95.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+Created by:
+    Ahmed Naeem (100602243)
+    Paljor ()
+    Ethan ()
+    Pranav ()
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //**********Ahmed***********
+        //Your code here
+        //**********Ahmed***********
+
+
+
+        //**********Paljor***********
+        //Your code here
+        //**********Paljor***********
+
+
+
+        //**********Ethan***********
+        //Your code here
+        //**********Ethan***********
+
+
+
+        //**********Pranav***********
+        //Your code here
+        //**********Pranav***********
+    }
+
+    //**********Ahmed***********
+        //Your code here
+    //**********Ahmed***********
+
+
+
+    //**********Paljor***********
+        //Your code here
+    //**********Paljor***********
+
+
+
+    //**********Ethan***********
+        //Your code here
+    //**********Ethan***********
+
+
+
+    //**********Pranav***********
+        //Your code here
+    //**********Pranav***********
+
+}
