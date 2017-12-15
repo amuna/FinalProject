8b516c7c919060f45b2538b96b5c9649c18a6687
gappaljor <tsering.paljor@uoit.net>
2017-12-15 11:22:03 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/amuna95/finalproject/BarberDBHelper.java
+++ app/src/main/java/com/example/amuna95/finalproject/BarberDBHelper.java
@@ -24,7 +24,11 @@ import java.util.List;
 
 public class BarberDBHelper extends SQLiteOpenHelper {
     private static boolean loginStatus = false;
-    static final int DATABASE_VERSION = 4;
+    private static int currentUser;
+    private static final int BARBER = 0;
+    private static final int USER = 1;
+    private static final int noUSER = 13;
+    static final int DATABASE_VERSION = 3;
     static final String TABLE = "Person";
     static final String CREATE_STATEMENT = "CREATE TABLE Person(\n" +
             " email varchar(100) primary key,\n" +
@@ -35,9 +39,7 @@ public class BarberDBHelper extends SQLiteOpenHelper {
             " postalCode varchar(10) not null,\n" +
             " city varchar(20),\n" +
             " address varchar(100),\n" +
-            " numRating int,\n" +
             " rating float,\n" +
-            " reviews varchar(1000),\n" +
             " storeName varchar(100),\n" +
             " description varchar(300),\n" +
             " phone varchar(11),\n" +
@@ -65,11 +67,9 @@ public class BarberDBHelper extends SQLiteOpenHelper {
 
     public void sampleBarbers() {
         createBarber("Ahmed Naeem", "anaeem@gmail.com", "blabla", "Oshawa", "160 Ice",
-                "Ahmed's Store", "The best place to get your cut",
-                "L1L0H1", "647-555-2222", 0, "");
-        createBarber("Emily Rosee", "emiy@emili.com", "blabla", "Ajax",
-                "99 Home Ave", "Top Cuts", "Specializing in women hair",
-                "L2F8G3", "773-333-2222", 0, "");
+                "Ahmed's Store", "The best place to get your cut", "L1L0H1", "647-555-2222");
+        createBarber("Emily Rosee", "emiy@emili.com", "blabla", "Ajax", "99 Home Ave",
+                "Top Cuts", "Specializing in women hair", "L2F8G3", "773-333-2222");
     }
 
     // CREATE
@@ -81,11 +81,9 @@ public class BarberDBHelper extends SQLiteOpenHelper {
                                  String storeName,
                                  String description,
                                String postalCode,
-                               String phone,
-                               int numRating,
-                               String reviews) {
+                               String phone) {
         // create a new entity object (Product)
-        Barber barber = new Barber(name, email, address, city, storeName, description, postalCode, phone, numRating, reviews);
+        Barber barber = new Barber(name, email, address, city, storeName, description, postalCode, phone,0,"");
         //Log.i("PRICE1", String.valueOf(price));
 
         // put that data into the database
@@ -100,8 +98,6 @@ public class BarberDBHelper extends SQLiteOpenHelper {
         newValues.put("description", description);
         newValues.put("personType", "B");
         newValues.put("rating", 0);
-        newValues.put("numRating", 0);
-        newValues.put("reviews", "");
         newValues.put("phone", phone);
         newValues.put("storeName", storeName);
         newValues.put("postalCode", postalCode);
@@ -147,8 +143,7 @@ public class BarberDBHelper extends SQLiteOpenHelper {
         Barber barber = null;
 
         SQLiteDatabase db = this.getReadableDatabase();
-        String[] columns = new String[] {"name", "rating", "address", "city", "description", "postalCode", "storeName", "phone",
-        "numRating", "reviews"};
+        String[] columns = new String[] {"name", "rating", "address", "city", "description", "postalCode", "storeName", "phone"};
         String where = "email = ?";
         String[] whereArgs = new String[] { "" + email };
         Cursor cursor = db.query(TABLE, columns, where, whereArgs, "", "", "");
@@ -164,11 +159,8 @@ public class BarberDBHelper extends SQLiteOpenHelper {
             String postalCode = cursor.getString(5);
             String storeName = cursor.getString(6);
             String phone = cursor.getString(7);
-            int numRating = cursor.getInt(8);
-            String reviews = cursor.getString(9);
 
-            barber = new Barber(name, email, address, city, storeName, description, postalCode, phone,
-                    numRating, reviews);
+            barber = new Barber(name, email, address, city, storeName, description, postalCode, phone,0,"");
             barber.setRating(rating);
             //contact.setId(id);
         }
@@ -195,37 +187,22 @@ public class BarberDBHelper extends SQLiteOpenHelper {
             String postalCode = cursor.getString(2);
 
             user = new User(name, email, postalCode);
+
+            //contact.setId(id);
         }
+
+        //Log.i("SQLite", "createContact(): " + contact);
 
         return user;
     }
 
-    public boolean updateBarber(Barber barber) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues newValues = new ContentValues();
-        newValues.put("name", barber.getName());
-        newValues.put("city", barber.getCity());
-        newValues.put("address", barber.getAddress());
-        newValues.put("description", barber.getDescription());
-        newValues.put("rating", barber.getRating());
-        newValues.put("phone", barber.getPhone());
-        newValues.put("storeName", barber.getStoreName());
-        newValues.put("postalCode", barber.getPostalCode());
-
-        int numRows = db.update(TABLE, newValues, "email = ?", new String[] { "" + barber.getEmail() });
-
-        Log.i("UPDATE", "YAMONNNN");
-        return (numRows == 1);
-
-    }
 
     public ArrayList<Barber> getAllBarbers() {
         ArrayList<Barber> barbers = new ArrayList<>();
 
         SQLiteDatabase db = this.getReadableDatabase();
         String[] columns = new String[]{"email", "name", "rating", "address", "city", "description",
-                "postalCode", "storeName", "phone", "price", "numRating", "reviews"};
+                "postalCode", "storeName", "phone", "price"};
         String where = "personType = ?";
         String[] whereArgs = new String[]{"" + "B"};
         Cursor cursor = db.query(TABLE, columns, where, whereArgs, "", "", "rating");
@@ -243,11 +220,8 @@ public class BarberDBHelper extends SQLiteOpenHelper {
                 String postalCode = cursor.getString(6);
                 String storeName = cursor.getString(7);
                 String phone = cursor.getString(8);
-                int numRating = cursor.getInt(9);
-                String reviews = cursor.getString(10);
 
-                Barber barber = new Barber(name, email, address, city, storeName, description,
-                        postalCode, phone, numRating, reviews);
+                Barber barber = new Barber(name, email, address, city, storeName, description, postalCode, phone,0,"");
                 barber.setRating(rating);
 
                 barbers.add(barber);
@@ -273,13 +247,19 @@ public class BarberDBHelper extends SQLiteOpenHelper {
 
     public boolean login(String userEmail, String password){
         SQLiteDatabase db = this.getReadableDatabase();
-        String query = "SELECT email, password from "+TABLE;
+        String query = "SELECT email, password, personType from "+TABLE;
         Cursor cursor =db.rawQuery(query, null);
 
         if(cursor.moveToFirst()){
             do{
                 if(userEmail.equals(cursor.getString(0)) && password.equals(cursor.getString(1))){
                     setLoginStatus(true);
+                    if(cursor.getString(2).equals("B")){
+                        currentUser = BARBER;
+                    }
+                    else if(cursor.getString(2).equals("U")){
+                        currentUser = USER;
+                    }
                     return true;
                 }
             }while (cursor.moveToNext());
@@ -305,6 +285,23 @@ public class BarberDBHelper extends SQLiteOpenHelper {
     }
     public boolean getLoginStatus(){
         return loginStatus;
+    }
+
+        //  Current logged in user
+        //  0 = barber
+        //  1 = user
+    public int getCurrentUser(){
+        if(getLoginStatus()){
+            return currentUser;
+        }
+        return noUSER;
+    }
+    public boolean logout(){
+        if(getLoginStatus()){
+            setLoginStatus(false);
+            return true;
+        }
+        return false;
     }
     // DELETE
     /*
